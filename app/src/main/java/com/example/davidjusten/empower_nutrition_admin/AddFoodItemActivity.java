89290ea6ec126aa1862281bba49b304d945ceca5
Dave <davejusten03@yahoo.com>
2018-03-12 09:16:36 -0400

    add item image uploads to firebase

--- app/src/main/java/com/example/davidjusten/empower_nutrition_admin/AddFoodItemActivity.java
+++ app/src/main/java/com/example/davidjusten/empower_nutrition_admin/AddFoodItemActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AddFoodItemActivity extends AppCompatActivity {
 private static final String LOG_TAG = AddFoodItemActivity.class.getSimpleName();
     private ImageButton mFoodImageButton;
     private static final int GALLREQ = 1;
     private EditText mName, mDescription, mPrice;
-    private Uri uri = null;
+    private Uri uri;
+    private StorageReference mStorageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,7 @@ private static final String LOG_TAG = AddFoodItemActivity.class.getSimpleName();
         mName = findViewById(R.id.itemName);
         mDescription = findViewById(R.id.itemDescrip);
         mPrice = findViewById(R.id.itemPrice);
+        mStorageReference = FirebaseStorage.getInstance().getReference("Item");
     }
 
     public void imageButtonClicked(View view) {
@@ -42,5 +52,21 @@ private static final String LOG_TAG = AddFoodItemActivity.class.getSimpleName();
     }
 
     public void addItemButtonClicked(View view) {
+
+        String name_text = mName.getText().toString().trim();
+        String desc_text = mDescription.getText().toString().trim();
+        String price_text = mPrice.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(name_text) && !TextUtils.isEmpty(desc_text) && !TextUtils.isEmpty(price_text)){
+            Log.i(LOG_TAG,"check1 ----------------- ");
+            StorageReference filepath = mStorageReference.child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    final Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Toast.makeText(AddFoodItemActivity.this, "Image uploaded", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
